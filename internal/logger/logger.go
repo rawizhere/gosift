2115ab1e8cac0baec850e0b109ad9ef5fa377f2b
rawizhere <burdanox@yandex.ru>
2026-08-31 01:11:34 +0300

    feat: slog logger

---
+++ internal/logger/logger.go
@@ -0,0 +1,30 @@
+package logger
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/rawizhere/gosift/internal/config"
+)
+
+func New(cfg *config.Config) *slog.Logger {
+	var level slog.Level
+	switch cfg.LogLevel {
+	case "debug":
+		level = slog.LevelDebug
+	case "warn":
+		level = slog.LevelWarn
+	case "error":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+	opts := &slog.HandlerOptions{Level: level}
+	var handler slog.Handler
+	if cfg.LogFormat == "json" {
+		handler = slog.NewJSONHandler(os.Stdout, opts)
+	} else {
+		handler = slog.NewTextHandler(os.Stdout, opts)
+	}
+	return slog.New(handler)
+}
